2655dcce9fbd0e8033af39217e72b5c1367dc165
gabriel-nt <gabrielnt.dev@gmail.com>
2022-05-02 22:37:48 -0300

    First class

---
+++ web/src/components/Widget.tsx
@@ -0,0 +1,26 @@
+import { ChatTeardropDots } from 'phosphor-react';
+import { Popover, Transition } from '@headlessui/react';
+import { useState } from 'react';
+
+export const Widget = () => {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen;
+  }
+
+  return (
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>Hello world</Popover.Panel>
+
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6" />
+
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-400 ease-linear">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
+  );
+};
